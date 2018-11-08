1e8212033cb60ed91b3ef6ced441928c9153b5f2
Adam <Adam.Whitehurst@live.com>
2018-11-08 12:37:19 -0800

    Add header, fix imports CategoriesScreen

--- components/screens/categories-screen.js
+++ components/screens/categories-screen.js
@@ -1,17 +1,40 @@
 import React, { Component } from "react";
 import { View, Text } from "react-native";
+import {
+  Icon,
+  FormValidationMessage,
+  ListItem,
+} from "react-native-elements";
 import { Theme } from "../../constants/theme";
-import { COLOR_TRAITS } from "../../constants/trait-categories";
+import {
+  COLOR_TRAITS,
+  SIZE_TRAITS,
+  SPECIES_TYPES
+} from "../../constants/trait-categories";
 
 export default class CategoriesScreen extends Component {
   constructor(props) {
     super(props);
+
+    console.log(COLOR_TRAITS);
+  }
+
+  async searchCategories () {
+
   }
 
   render() {
     return (
       <View style={Theme.containerContainer}>
-        <Text> YOOOOOOOOOOOOOOO </Text>
+        <View style={Theme.headerContainer}>
+          <Text style={Theme.headerTitle}>CATEGORIES</Text>
+          <Icon
+            iconStyle={Theme.headerButton}
+            name="search"
+            color="#fff"
+            onPress={() => this.searchCategories()}
+          />
+        </View>
       </View>
     );
   }
