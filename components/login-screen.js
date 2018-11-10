3bdfa5473ed15040694236d46c3d1799e316f11b
Adam <Adam.Whitehurst@live.com>
2018-11-10 10:27:46 -0800

    Clean up login-screen

--- components/login-screen.js
+++ components/login-screen.js
@@ -5,43 +5,9 @@ import { Authenticator } from "aws-amplify-react-native";
 export default class LoginScreen extends Component {
   constructor(props) {
     super(props);
-
-    this.state = {
-      text: "default"
-    };
   }
 
   render() {
     return <Authenticator />;
   }
 }
-
-const styles = StyleSheet.create({
-  containerContainer: {
-    height: "100%",
-    width: "100%"
-  },
-  contentContainer: {
-    flex: 10,
-    alignItems: "center",
-    justifyContent: "flex-start",
-    backgroundColor: "skyblue"
-  },
-  headerContainer: {
-    flex: 1,
-    flexDirection: "row",
-    flexWrap: "nowrap",
-    alignItems: "center",
-    justifyContent: "center",
-    backgroundColor: "brown"
-  },
-  headerTitle: {
-    flex: 5,
-    textAlign: "center"
-  },
-  headerButton: {
-    flex: 1,
-    marginTop: 10,
-    padding: 10
-  }
-});
