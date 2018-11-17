cea4350a8415836faaed650118cf155b34c3e8b5
Adam <Adam.Whitehurst@live.com>
2018-11-17 13:03:28 -0800

    Programmatic state and fieldInput instantiation :scream: :sunglasses:

--- components/new-species-screen.js
+++ components/new-species-screen.js
@@ -2,14 +2,14 @@ import React, { Component } from 'react';
 import { View, Text, TextInput } from 'react-native';
 import { BaseTheme, NewSpeciesTheme, FieldInputTheme, EditModalTheme } from '../constants/theme';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
-
+import {DetailFields, NondetailFields, ExcludedNewSpeciesNondetailFields, FieldInputPlaceholders } from '../constants/species-fields';
 class FieldInput extends Component {
   render() {
     return (
       <View style={[EditModalTheme.content, BaseTheme.row]}>
         <View style={FieldInputTheme.label}>
           <Text style= {[FieldInputTheme.labelText, BaseTheme.centerText, BaseTheme.bold]}>
-          HELLO WORLD
+          {this.props.fieldName.toProperCase()}
           </Text>
         </View>
         <View style={EditModalTheme.fieldInputContainer}>
@@ -18,7 +18,9 @@ class FieldInput extends Component {
             numberOfLines={1}
             returnKeyType={'done'}
             returnKeyLabel={'done'}
-            blurOnSubmit={true} />
+            blurOnSubmit={true}
+            value={this.props.fieldValue}
+            onChangeText={(text) => {this.props.onChangeText(this.props.fieldName, text)}}/>
         </View>
       </View>
     );
@@ -28,28 +30,58 @@ class FieldInput extends Component {
 class NewSpeciesScreen extends Component {
   constructor(props) {
     super(props);
-    this.state = {  };
+    this.renderFieldInputs = this.renderFieldInputs.bind(this);
+
+    let state = {};
+    state.fields = {};
+    for (field of NondetailFields) {
+      if (ExcludedNewSpeciesNondetailFields.includes(field) == false) {
+        state.fields[field] = FieldInputPlaceholders[field] || '';
+      }
+    }
+    for (field of DetailFields) {
+      state.fields[field] = FieldInputPlaceholders[field] || '';
+    }
+    console.log(state.fields);
+    this.state = state;
   }
+  setFieldValue (fieldName, newFieldValue) {
+    console.log(fieldName + ': ' + newFieldValue);
+    this.setState((prevState) =>{
+      let newFields = prevState.fields;
+      newFields[fieldName] = newFieldValue;
+      return {feilds: newFields};
+    });
+  }
+
+  renderFieldInputs() {
+    let fields = Object.keys(this.state.fields);
+    let renderedFields = [];
+
+    for (field of fields) {
+      renderedFields.push(
+        <FieldInput
+        key={field}
+        fieldName={field}
+        fieldValue={this.state.fields[field]}
+        onChangeText={(fieldName, newFieldValue) =>
+          {this.setFieldValue(fieldName, newFieldValue)}}/>
+      )
+    }
+    return renderedFields;
+  }
+
   render() {
     return (
       <KeyboardAwareScrollView>
         <View style={EditModalTheme.header}>
           <Text style={[EditModalTheme.headerText]}>New Species</Text>
         </View>
-      <FieldInput/>
-      <FieldInput/>
-      <FieldInput/>
-      <FieldInput/>
-      <FieldInput/>
-      <FieldInput/>
-      <FieldInput/>
-      <FieldInput/>
-      <FieldInput/>
-      <FieldInput/>
-      <FieldInput/>
-      <FieldInput/>
-      <FieldInput/>
-      <FieldInput/>
+        {this.renderFieldInputs()}
+      {/* <FieldInput
+        fieldName={'overview'}
+        fieldValue={this.state['overview']}
+        onChangeText={(fieldName, newFieldValue) => {this.setFieldValue(fieldName, newFieldValue)}}/> */}
       </KeyboardAwareScrollView>
     );
   }
--- constants/species-fields.js
+++ constants/species-fields.js
@@ -12,7 +12,19 @@ export const NondetailFields = [
   'id',
   'aliases',
   'alias',
+  'sciname',
 ];
+
+export const ExcludedNewSpeciesNondetailFields = [
+  'url',
+  'id',
+  'aliases',
+];
+
+export const FieldInputPlaceholders = {
+  alias: 'The common name of the species',
+  overview: 'It does stuff sometimes.',
+};
 
 // https://stackoverflow.com/questions/196972/convert-string-to-title-case-with-javascript
 String.prototype.toProperCase = function () {
