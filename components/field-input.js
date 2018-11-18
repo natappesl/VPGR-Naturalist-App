1a93d1d5c63bd2d4904cf2044d5ac03cdda0d74f
Adam <Adam.Whitehurst@live.com>
2018-11-18 10:12:35 -0800

    Implement EntryInput

--- components/field-input.js
+++ components/field-input.js
@@ -1,13 +1,12 @@
 import React, { Component } from "react";
 import { View, Text, TextInput } from "react-native";
-import { BaseTheme, FieldInputTheme } from "../constants/theme";
+import { BaseTheme, FieldInputTheme, Colors } from "../constants/theme";
+import { Icon } from 'react-native-elements';
 
 export class FieldInput extends Component {
   render() {
     return (
-      <View
-        style={[FieldInputTheme.container, BaseTheme.row, BaseTheme.shadow]}
-      >
+      <View style={[FieldInputTheme.container, BaseTheme.row, BaseTheme.shadow]}>
         <View style={FieldInputTheme.label}>
           <Text
             style={[
@@ -20,7 +19,8 @@ export class FieldInput extends Component {
           </Text>
         </View>
         <View style={[FieldInputTheme.inputContainer]}>
-          <TextInput
+          {this.props.children ? (this.props.children) : (
+            <TextInput
             style={[FieldInputTheme.input]}
             multiline={true}
             numberOfLines={1}
@@ -33,7 +33,54 @@ export class FieldInput extends Component {
               this.props.onChangeText(this.props.fieldName, text);
             }}
           />
+          )}
         </View>
+      </View>
+    );
+  }
+}
+
+export class EntryInput extends Component {
+  constructor (props) {
+    super(props);
+  }
+
+  render () {
+    let renderedEntries = [];
+
+    for (i = 0; i < this.props.entries.length; i++) {
+      let index = i;
+      renderedEntries.push (
+        <View key={index} style={{flex:1,flexDirection: 'row',}}>
+        <FieldInput
+          fieldName={this.props.entryName + ' ' + (index + 1)}
+          fieldValue={this.props.entries[index]}
+          onChangeText={(fieldName, newFieldValue) => {
+            this.props.onUpdate(index, newFieldValue);
+          }}
+        />
+        </View>
+      );
+    }
+
+    return (
+      <View style={[FieldInputTheme.container, BaseTheme.shadow]}>
+        <View style={[FieldInputTheme.label, BaseTheme.row]}>
+          <Text style={[BaseTheme.headerTitle, BaseTheme.centerText, BaseTheme.bold, FieldInputTheme.labelText]}>{this.props.entryName}</Text>
+          <Icon
+          containerStyle={{padding:5,}}
+          name='plus'
+          type='font-awesome'
+          color={Colors.positive}
+          onPress={() => {this.props.onAdd()}}/>
+          <Icon
+          containerStyle={{padding:5,}}
+          name='remove'
+          type='font-awesome'
+          color={Colors.negative}
+          onPress={() => {this.props.onDelete()}}/>
+        </View>
+      {renderedEntries}
       </View>
     );
   }
